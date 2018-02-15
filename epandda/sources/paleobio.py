526e02322af333a33b4f3fe825bcb91664c62c80
mwbenowitz <mwbenowitz@gmail.com>
2018-02-15 16:49:50 -0500

    Fixed issue with geo_point searches

--- epandda/sources/paleobio.py
+++ epandda/sources/paleobio.py
@@ -10,10 +10,10 @@ class paleobio:
 "abund_value", "abund_unit", "reference_no" "occ_reference_no",
 "comments", "taxon_no", "country", "state", "cc1", "collection_aka", "collection_name", "research_group",
 "min_ma", "max_ma", "abund_unit", "diet", "vision", "member",
-"early_interval", "late_interval", "geogcomments", "phylum", "startgroup", "life_habit", 
+"early_interval", "late_interval", "geogcomments", "phylum", "startgroup", "life_habit",
 "emllocage_min", "emllocage_max", "locage_min", "locage_max", "zone", "formation",
 "member", "lithadj", "lithification", "minor_lithology", "lithology1", "lithology2", "lithadj2",
 "lithification2", "minor_lithology2", "environment", "tectonic_setting", "geology_comments",
 "taxonomy_comments", "collection_comments", "collectors", "collection_type", "assembl_comps",
 "preservation_comments", "pres_mode", "lithdescript", "stratcomments", "stratscale", "geogcomments",
-"paleolng", "paleolat", "plate", "latlng_precision", "lat", "lng", "reference_no" "coll_reference_no", "collection_no"]
+"paleolng", "paleolat", "plate", "latlng_precision", "lat", "lng", "reference_no" "coll_reference_no", "collection_no", "geoPoint", "paleoGeoPoint"]
