7378914ee3f61b218725f7314c180366f186cecb
Seth <seth@whirl-i-gig.com>
2019-12-23 15:12:15 -0500

    Add "debug" config option; all debug mode to disable caching

--- helpers/cache_helpers.py
+++ helpers/cache_helpers.py
@@ -1,5 +1,10 @@
 from flask import Flask, request, current_app, make_response, session, escape, Response
 import hashlib
+import json
+
+config = json.load(open('./config.json'));
+
+
 #
 # Extend make_cache_key from flask_caching to enable cache keys to be set on api parameters
 #
@@ -16,6 +21,11 @@ def make_cache_key(*args, **kwargs):
 def bypass_caching(*args, **kwargs):
     path = request.path
     args = request.args
+    
+    if 'debug' in config and config['debug'] == True:
+    	return True
+    	
     if 'skipCache' in args and (args['skipCache'] == '1' or args['skipCache'].lower() == 'true'):
         return True
+        
     return False
